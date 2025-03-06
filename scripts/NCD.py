6b477c88ffbddef3d9375617c6acbe15c896324e
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2025-03-06 09:48:08 -0500

    Update NCD calculation

--- scripts/NCD.py
+++ scripts/NCD.py
@@ -2,5 +2,5 @@ import numpy as np
 import math
 # NCD function from eq. 1 in Bitarello, B. D. et al. Signatures of long-term balancing selection in human genomes. Genome Biol. Evol. 10, 939–955 (2018).
 def ncd(MAF, TF):
-    ncd = math.sqrt(sum(TF-MAF)**2/len(MAF)) 
+    ncd = math.sqrt(sum((TF-MAF)**2)/len(MAF)) 
     return(ncd)
